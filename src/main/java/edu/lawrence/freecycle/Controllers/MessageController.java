5ad34294950adeaa0ff50bda85d00a9f69f33e60
Duong Nguyen (Sunny) <nttduong100205@gmail.com>
2026-04-29 11:48:40 -0500

    Update MessageController.java

--- src/main/java/edu/lawrence/freecycle/Controllers/MessageController.java
+++ src/main/java/edu/lawrence/freecycle/Controllers/MessageController.java
@@ -1,5 +1,30 @@
 package edu.lawrence.freecycle.Controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import edu.lawrence.freecycle.Classes.Message;
+import edu.lawrence.freecycle.Repositories.MessageDAO;
+
+@RestController
+@RequestMapping("/messages")
+@CrossOrigin(origins="*")
 public class MessageController {
 
+    private final MessageDAO dao;
+
+    public MessageController(MessageDAO dao) {
+        this.dao = dao;
+    }
+
+    @PostMapping
+    public void saveMessage(@RequestBody Message message) {
+        dao.save(message);
+    }
+
+    @GetMapping("/{receiverId}")
+    public List<Message> getMessages(@PathVariable int receiverId) {
+        return dao.findByReceiver(receiverId);
+    }
 }
